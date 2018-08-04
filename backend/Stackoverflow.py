1365bb31bd9ad52473caa3f604d1fe752e2cd008
Rohan Kumar <rohan.carmel@gmail.com>
2018-08-05 03:29:02 +0530

    added related tags

---
+++ backend/Stackoverflow.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+class Stackoverflow:
+
+    def __init__(self):
+        print('Stackoverflow')
+
+    def parseResponse(self,json):
+        tags = []
+        items = json['items']
+        for item in items:
+            tags.append(item['name'])
+        return tags
+
+    def callStackoverflow(self,query,number_results):
+        # https://api.stackexchange.com/2.2/tags/typescript/related?page=1&pagesize=5&site=stackoverflow
+        url = "https://api.stackexchange.com/2.2/tags/" + query + "/related?page=1&pagesize=" + number_results + "&site=stackoverflow"
+        response = requests.get(url)
+        return response.text
+
+    def getRelatedTags(self,query):
+        response = self.callStackoverflow(query)
+        relatedTags = self.parseResponse(json.loads(response))
+        return relatedTags
