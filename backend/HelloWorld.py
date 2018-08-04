31a6c494efd4428d4d576c4906cdaafce60153bf
abhishekori <abhikori1994@gmail.com>
2018-08-05 03:36:39 +0530

    Added hello world links

---
+++ backend/HelloWorld.py
@@ -0,0 +1,27 @@
+
+from GoogleSearch import GoogleSearch
+gs=GoogleSearch()
+
+class HelloWrold:
+
+
+
+    def __init__(self):
+        print("hello world class")
+
+
+    def getHelloWorlds(self,key):
+        sarchQuery=key+" Hello World program"
+        helloWorlds=gs.search(sarchQuery,3)
+        return helloWorlds
+
+
+
+
+# hw=HelloWrold()
+# print(hw.getHelloWorlds('java'))
+
+
+
+    
+
